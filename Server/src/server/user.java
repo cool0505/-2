89291b2a5ab5c5db42459ddb56fe4265bf86705c
cool0505 <joowonkoh@naver.com>
2020-11-17 20:12:53 +0900

    user 클래스 변경

--- Server/src/server/user.java
+++ Server/src/server/user.java
@@ -2,4 +2,37 @@ package server;
 
 public class user {
 
+	
+	String classof;
+	String name;
+	String id;
+	String pw;
+	
+	public String getClassof() {
+		return name;
+	}
+	public void setClassof(String classof) {
+		this.classof = classof;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getID() {
+		return id;
+	}
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+	public String getPW() {
+		return pw;
+	}
+	public void setPW(String pw) {
+		this.pw = pw;
+	}
 }
