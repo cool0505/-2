e5f4edd9d49fca97614f033aff88eefb7a345827
cool0505 <joowonkoh@naver.com>
2020-11-18 23:00:51 +0900

    서버와의 소켓 통신부분

--- Client/src/UNIV_PASS/IDcard_detector.java
+++ Client/src/UNIV_PASS/IDcard_detector.java
@@ -4,9 +4,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -36,10 +42,13 @@ class frame2 extends JFrame{
 	}
 }
 public class IDcard_detector {
- 
+	static String[] strcomp = new String[2]; 
+	static int count=0;
+	static Socket socket;
 	static frame2 frame;
 	static JLabel lbl;
 	static JLabel lbl2;
+	static JLabel lbl3;
 	static JPanel pn1;
 	static JPanel pn2;
 	static JButton bt1;
@@ -64,6 +73,15 @@ public class IDcard_detector {
 
 	}
 	public static void main(String[] args) {
+		try {
+			socket = new Socket("192.168.1.6", 8282);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
@@ -84,7 +102,7 @@ public class IDcard_detector {
 				Mat frameCapture = new Mat();
 				videoDevice.read(frameCapture);
 				Mat subimage = null;
-			
+				Mat OCRimage = null;
 				MatOfRect faces = new MatOfRect();
 				//Rect rect=null;
 				cascadeFaceClassifier.detectMultiScale(frameCapture,faces);								
@@ -98,13 +116,15 @@ public class IDcard_detector {
 				for (Rect rect : faces.toArray()) {
 					if((sum=rect.width+rect.height)>600) {
 						System.out.println(rect.width+rect.height);
-						Imgproc.putText(frameCapture, "S ID C", new Point(rect.x,rect.y-5), 1, 2, new Scalar(0,0,255));								
-						Imgproc.rectangle(frameCapture, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+						Imgproc.putText(frameCapture, "STUDENT ID CARD", new Point(rect.x,rect.y+55), 1, 2, new Scalar(0,100,100));								
+						Imgproc.rectangle(frameCapture, new Point(rect.x, rect.y+50), new Point(rect.x + rect.width, rect.y + rect.height-50),
 						new Scalar(0, 100, 0),3);
 						LIVEPushImage(ConvertMat2Image(frameCapture));
 						//System.out.println(process("Img5.jpg"));
-				        Rect rectCrop=new Rect(new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height));
-				        subimage = new Mat(frameCapture ,rectCrop);
+				        Rect rectCrop=new Rect(new Point(rect.x, rect.y+50), new Point(rect.x + rect.width, rect.y + rect.height-50));
+				        Rect rectCrop2=new Rect(new Point(0, 50), new Point(rect.width, rect.height-180));
+				        subimage = new Mat(frameCapture ,rectCrop);				        
+				        OCRimage = new Mat(subimage ,rectCrop2);
 					}
 				}	
 				
@@ -146,7 +166,8 @@ public class IDcard_detector {
 				if(sum>600) {
 
 					SubPushImage(ConvertMat2Image(subimage));
-					ResizeImage(subimage);
+					//SubPushImage(ResizeImage(OCRimage));
+					ResizeImage(OCRimage);
 				}
 				//System.out.println(String.format("%s y�z(FACES)", faces.toArray().length));
 			}
@@ -200,18 +221,53 @@ public class IDcard_detector {
 		// Rect rectCrop=new Rect(new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height));
 		String result ="";
 		try {
-			Imgproc.resize(asd,last,new Size(1200,1200));
+			Imgproc.resize(asd,last,new Size(1600,800));
 			Image img4 = ConvertMat2Image(last);
 			result =instance.doOCR((BufferedImage)img4);
-			
 			String resultF=result.substring(result.indexOf("20"), result.indexOf("20")+10);
 			System.out.println(resultF);
+			if(count==2) {
+				if(strcomp[0].equals(strcomp[1])) {
+					System.out.println("전송");
+					String sendstring = "1/" + strcomp[1];
+					InputStreamReader ISR = new InputStreamReader(socket.getInputStream());
+					PrintWriter print = new PrintWriter(socket.getOutputStream());          
+		            print.println(sendstring);
+		            print.flush();
+		            BufferedReader buffer = new BufferedReader(ISR);
+		               
+		            String LoginResult = buffer.readLine();
+		            System.out.println(LoginResult);
+		            lbl3.setText(LoginResult);
+		            Thread.sleep(3000);
+		            lbl3.setText("학생증을 인식해주세요");
+
+		            count=0;
+		            strcomp[0]=null;
+		            strcomp[1]=null;
+				}
+				else {
+					String buf=strcomp[0];
+					strcomp[0]=strcomp[1];
+					strcomp[1]=resultF;
+					System.out.println("빠꿈");
+				}
+				System.out.println("빠꿈2");
+				
+				
+			}
+			else {
+				System.out.println(count);
+				strcomp[count]=resultF;	
+				count++;
+			}
 		}
 		catch(NullPointerException e) {			
 		}
 		catch(Exception e) {
-			e.printStackTrace();
+
 		}
+
 		
 
 	}
@@ -226,7 +282,12 @@ public class IDcard_detector {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null); 		   // 이렇게 컬러값을 생성 후     // 센 백그라운드에 넣어준다		  
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);  
+		frame.setVisible(true);
+		lbl3 = new JLabel("학생증을 인식해주세요");
+        lbl3.setLayout(null);
+  		lbl3.setBounds(700, 335, 450, 270);
+  		frame.getContentPane().add(lbl3);
+  		lbl3.setVisible(true);
 	}
 
 	/**
@@ -246,20 +307,15 @@ public class IDcard_detector {
 						
 		}
 		lbl = new JLabel(new ImageIcon(img2));
+
 		lbl.setOpaque(true); 
 		lbl.setBackground(Color.BLACK);
 		lbl.setLayout(null);
 		lbl.setBounds(28, 80, 645, 385);
+		frame.getContentPane().add(lbl);	
 
-
-
-		frame.getContentPane().add(lbl);
-
-		
 		lbl.revalidate(); 
-
 		lbl.repaint(); 
-
 	}
 	public static void SubPushImage(Image img3) {
 
@@ -280,12 +336,7 @@ public class IDcard_detector {
 		lbl2.revalidate();
 
 		lbl2.repaint();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 
 	}
 	
